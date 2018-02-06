79ddbdc110dd0e2d9132fcfbbf8d363a526e59c9
Jacob <jake.laurenti@gmail.com>
2018-02-05 22:55:11 -0500

    API Controller working

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,41 @@
+class API::EventsController < ApplicationController
+	skip_before_action :verify_authenticity_token
+
+	before_action :set_access_control_headers
+
+	skip_before_action :authenticate_user!
+
+	def set_access_control_headers
+		headers['Access-Control-Allow-Origin'] = '*'
+		headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+		headers['Access-Control-Allow-Headers'] = 'Content-Type'
+	end
+
+	def create
+    registered_application = Registeredapp.find_by(url: request.env['HTTP_ORIGIN'])
+
+    if registered_application.nil?
+      render json: { error: "missing valid registered application URL" }, status: :unprocessable_entity
+    else
+      @event = registered_application.events.build( event_params )
+        if @event.save
+          render json: @event, status: :created
+        else
+          render json: { error: "missing event name" }, status: :unprocessable_entity
+        end
+    end
+  end
+
+	# Test: curl -v -H "Accept: application/json" -H "Origin: http://nba.com" -H "Content-Type: application/json" -X POST -d '{"name":"test event"}'  http://localhost:3000/api/events
+
+
+	def preflight
+		head 200
+	end
+
+	private
+	def event_params
+		params.require(:event).permit(:name)
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
